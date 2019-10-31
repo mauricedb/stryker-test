309dfca9cff30ba814b49a7eb188a1ad9edee82e
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-10-31 11:35:13 +0100

    Label.displayName

--- src/shared/components/label/Label.test.tsx
+++ src/shared/components/label/Label.test.tsx
@@ -1,13 +1,3 @@
-import React, { ReactElement } from "react";
-import "@testing-library/jest-dom/extend-expect";
-import { render, fireEvent } from "@testing-library/react";
-import { Formik, FormikProps } from "formik";
-
-// import LabeledTextInput from "./LabeledTextInput";
-// import ErrorBoundaryContext from "../error-handling/ErrorBoundaryContext";
-// import { TextInput } from "..";
-// import TextInput from "./TextInput";
-
 import Label from "./Label";
 
 test("dummy", () => {});
