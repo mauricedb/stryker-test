74139f08fb119e13cf003a7df11120eec11191d3
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-10-31 10:14:36 +0100

    Clean tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,9 +3,3 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 test("dummy", () => {});
-
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
--- src/shared/components/error-handling/ErrorBoundaryContextProvider.test.tsx
+++ src/shared/components/error-handling/ErrorBoundaryContextProvider.test.tsx
@@ -4,15 +4,3 @@ import { render } from "@testing-library/react";
 import ErrorBoundaryContextProvider from "./ErrorBoundaryContextProvider";
 
 test("dummy", () => {});
-
-// describe("ErrorBoundaryContextProvider", (): void => {
-//   test("expect ErrorBoundaryContextProvider to render", (): void => {
-//     const { getByText } = render(
-//       <ErrorBoundaryContextProvider>
-//         <div>ErrorBoundaryContextProvider</div>
-//       </ErrorBoundaryContextProvider>
-//     );
-
-//     expect(getByText("ErrorBoundaryContextProvider")).toBeInTheDocument();
-//   });
-// });
